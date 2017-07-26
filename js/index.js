7e1a084ead79708f7fb166e290ca86a0b612afcf
DIEDIEKUN <lucklycc@126.com>
2017-07-26 21:43:14 +0800

    Tmall
    模仿tmall部分

---
+++ js/index.js
@@ -0,0 +1,22 @@
+﻿function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+function show() {
+    var menu= document.getElementsByClassName("eachMenu")[0];
+    var recom = document.getElementsByClassName("recommend")[0];
+    menu.onmouseenter = function () {
+        recom.style.visibility = "visible";
+    }
+    menu.onmouseleave=function(){
+        recom.style.visibility = "hidden";
+    }
+}
+addLoadEvent(show);
